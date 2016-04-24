e80bdf2d1b0e011b1af346059569097f74a36b1f
manuelzegarra <mzegarra@gmail.com>
2016-04-24 18:22:04 -0500

    Log4j2

--- net.msonic.gcm4j/net.msonic.gcm4j.web/src/test/java/net/msonic/gcm4j/web/ClienteControllerTest.java
+++ net.msonic.gcm4j/net.msonic.gcm4j.web/src/test/java/net/msonic/gcm4j/web/ClienteControllerTest.java
@@ -3,12 +3,15 @@ package net.msonic.gcm4j.web;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.web.client.RestTemplate;
 
@@ -17,15 +20,17 @@ import org.springframework.web.client.RestTemplate;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/applicationContext-test.xml"})
-public class ClienteControllerTest extends AbstractJUnit4SpringContextTests {
-
+public class ClienteControllerTest  {
+	
+	 private static final Logger LOG = LogManager.getLogger(ClienteControllerTest.class);
+	 
 	@Autowired
 	private RestTemplate restTemplate;
 	
 	
 	@Before
 	public void setUp() {
-		
+		LOG.debug("Start");
 	}
 	
 	@Test
@@ -37,7 +42,7 @@ public class ClienteControllerTest extends AbstractJUnit4SpringContextTests {
 	@Test
     public void testGetMessage() {
 		 
-		 String result = restTemplate.getForObject("http://192.168.0.13:9080/gcm4jweb/cliente/sendMail/{parametro}", String.class, "a");
+		 String result = restTemplate.getForObject("http://192.168.0.14:9080/gcm4jweb/cliente/sendMail/{parametro}", String.class, "a");
 		 assertThat( result , equalTo("A"));
     }
 }
