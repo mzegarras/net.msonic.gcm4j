be2453dce581b726e47633cd4e1936c4092640e5
manuelzegarra <mzegarra@gmail.com>
2016-04-24 12:06:13 -0500

    JMS Send And Recieve

--- net.msonic.gcm4j/net.msonic.gcm4j.web/src/main/java/net/msonic/gcm4j/web/ClienteController.java
+++ net.msonic.gcm4j/net.msonic.gcm4j.web/src/main/java/net/msonic/gcm4j/web/ClienteController.java
@@ -1,5 +1,8 @@
 package net.msonic.gcm4j.web;
 
+import java.util.UUID;
+
+import javax.jms.Destination;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.Session;
@@ -7,6 +10,7 @@ import javax.jms.TextMessage;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.jms.core.MessageCreator;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,22 +25,45 @@ public class ClienteController {
 	final static Logger logger = Logger.getLogger(ClienteController.class);
 
 	@Autowired
+	@Qualifier("jmsTemplate1")
 	JmsTemplate jmsTemplate;
+	
+	
+	@Autowired
+	@Qualifier("receiveDestination")
+	Destination receiveDestination;
+	
 
 	@RequestMapping(value = "/sendMail/{p1}", method = RequestMethod.GET)
 	public String send(final @PathVariable String p1) {
-
-		jmsTemplate.send(new MessageCreator() {
+		
+		UUID uuid = UUID.randomUUID();
+		final String randomUUIDString = uuid.toString();
+		
+		
+		Message response = jmsTemplate.sendAndReceive(new MessageCreator() {
 
 			public Message createMessage(Session session) throws JMSException {
 				// TODO Auto-generated method stub
 				TextMessage logMessage = session.createTextMessage();
 				logMessage.setText(p1);
+				logMessage.setJMSCorrelationID(randomUUIDString);
+				logMessage.setJMSReplyTo(receiveDestination);
 				return logMessage;
 			}
 
 		});
+		
+		String msg = "";
+		
+		try {
+			msg = ((TextMessage) response).getText();
+			logger.debug("Response:" + msg);
+		} catch (JMSException ex) {
+			// TODO Auto-generated catch block
+			logger.error("",ex);
+		}
 
-		return p1;
+		return msg;
 	}
 }
--- net.msonic.gcm4j/net.msonic.gcm4j.web/src/main/java/net/msonic/gcm4j/web/SendMailListener.java
+++ net.msonic.gcm4j/net.msonic.gcm4j.web/src/main/java/net/msonic/gcm4j/web/SendMailListener.java
@@ -3,24 +3,56 @@ package net.msonic.gcm4j.web;
 import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageListener;
+import javax.jms.Session;
 import javax.jms.TextMessage;
 
 import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 
 public class SendMailListener implements MessageListener {
 	
-	private static final Logger LOG = Logger.getLogger(SendMailListener.class);
+	final static Logger logger = Logger.getLogger(SendMailListener.class);
 	
-	public void onMessage(Message message) {
+	
+	@Autowired
+	@Qualifier("jmsTemplate2")
+	JmsTemplate jmsTemplate;
+	
+	public void onMessage(final Message message) {
 		// TODO Auto-generated method stub
 		
+		
 		 try {
-             LOG.info(((TextMessage) message).getText());
+             //LOG.info(((TextMessage) message).getText());
+             
+             final String msg = ((TextMessage) message).getText();
+             logger.info(msg);
+             
+             jmsTemplate.send(message.getJMSReplyTo(),new MessageCreator() {
+				
+				public Message createMessage(Session session) throws JMSException {
+					// TODO Auto-generated method stub
+					TextMessage logMessage = session.createTextMessage();
+					logMessage.setJMSCorrelationID(message.getJMSCorrelationID());
+					logMessage.setText(msg.toUpperCase());
+					return logMessage;
+				}
+			});
          }
          catch (JMSException ex) {
-        	 LOG.error("",ex);
-             throw new RuntimeException(ex);
+        	 logger.error("",ex);
          }
+		 
+		 
+		 /*
+		  Message response = session.createMessage();
+  response.setJMSCorrelationID(request.getJMSCorrelationID())
+ 
+  producer.send(request.getJMSReplyTo(), response)
+		  * */
 		
 		
 	}
