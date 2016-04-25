faa75c3aec8d6f52c6eb72d13991252299d32ad3
manuelzegarra <mzegarra@gmail.com>
2016-04-24 19:10:51 -0500

    Test Parametri

--- net.msonic.gcm4j/net.msonic.gcm4j.web/src/test/java/net/msonic/gcm4j/web/ClienteControllerTest.java
+++ net.msonic.gcm4j/net.msonic.gcm4j.web/src/test/java/net/msonic/gcm4j/web/ClienteControllerTest.java
@@ -4,21 +4,28 @@ import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import static org.hamcrest.CoreMatchers.*;
 
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.TestContextManager;
 import org.springframework.web.client.RestTemplate;
+import org.junit.runners.Parameterized;
 
 
 
-
-@RunWith(SpringJUnit4ClassRunner.class)
+//@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(Parameterized.class)
 @ContextConfiguration(locations = { "classpath:/applicationContext-test.xml"})
 public class ClienteControllerTest  {
 	
@@ -27,10 +34,13 @@ public class ClienteControllerTest  {
 	@Autowired
 	private RestTemplate restTemplate;
 	
-	
+	 // Manually config for spring to use Parameterised
+    private TestContextManager testContextManager;
+    
 	@Before
-	public void setUp() {
-		LOG.debug("Start");
+	public void setUp() throws Exception {
+		this.testContextManager = new TestContextManager(getClass());
+        this.testContextManager.prepareTestInstance(this);
 	}
 	
 	@Test
@@ -42,7 +52,32 @@ public class ClienteControllerTest  {
 	@Test
     public void testGetMessage() {
 		 
-		 String result = restTemplate.getForObject("http://192.168.0.14:9080/gcm4jweb/cliente/sendMail/{parametro}", String.class, "a");
-		 assertThat( result , equalTo("A"));
+		 //String result = restTemplate.getForObject("http://192.168.0.14:9080/gcm4jweb/cliente/sendMail/{parametro}", String.class, "a");
+		
+		 ResponseEntity<String> response = restTemplate.getForEntity("http://192.168.0.13:9080/gcm4jweb/cliente/sendMail/{parametro}", String.class, parametro);
+		 
+		 assertEquals(200, response.getStatusCode().value());
+	     assertNotNull(response.getBody());
+	     assertThat( response.getBody() , equalTo(esperado));
+	        
     }
+	
+	
+	@Parameter(value = 0)
+    public String parametro;
+
+    @Parameter(value = 1)
+    public String esperado;
+
+    
+	@Parameters
+    public static Collection<Object[]> data() {
+         Collection<Object[]> params = new ArrayList<Object[]>();
+         params.add(new Object[] { "a", "A"});
+         params.add(new Object[] { "b", "B"});
+         params.add(new Object[] { "c", "C"});
+         params.add(new Object[] { "d", "D"});
+
+         return params;
+     }
 }
