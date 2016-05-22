6c66e90fca460abce04086a8653252fe7c3d91ad
manuelzegarra <mzegarra@gmail.com>
2016-05-22 08:56:44 -0500

    Parser fecha y hora

--- net.msonic.gcm4j/net.msonic.gcm4j.web/src/test/java/net/msonic/gcm4j/web/ThymeleafTest.java
+++ net.msonic.gcm4j/net.msonic.gcm4j.web/src/test/java/net/msonic/gcm4j/web/ThymeleafTest.java
@@ -1,11 +1,20 @@
 package net.msonic.gcm4j.web;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.Locale;
 
 import static org.junit.Assert.*;
+
+import org.apache.commons.logging.Log;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -23,16 +32,47 @@ public class ThymeleafTest {
 
 	private static final Logger LOG = LogManager.getLogger(ThymeleafTest.class);
 	private static final Locale locale_default = Locale.ENGLISH;
-
+	private String htmlContent = null;
+	
+	private Document documentoHTML=null;
+	
 	@Autowired
 	private TemplateEngine templateEngine;
 
 	@Before
 	public void setUp() throws Exception {
-
+		LOG.debug("setUp");
+		
+		
+		final Context ctx = new Context(locale_default);
+		
+		String target = "27-09-1991 20:29:30";
+		DateFormat df = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+		Date fechaTransaccion =  df.parse(target);
+		
+		
+		TransaccionTO transaccionTO = new TransaccionTO();
+		transaccionTO.setName("Manuel");
+		transaccionTO.setLastName("Zegarra");
+		transaccionTO.setFecha(fechaTransaccion);
+		transaccionTO.setNumberTx("122");
+		transaccionTO.setCuentaCargo("1122333999");
+		transaccionTO.setCuentaCargoMoneda("804");
+		transaccionTO.setCuentaCargoAlias("Simple");
+		transaccionTO.setCuentaDestino("11223344557");
+		transaccionTO.setNombreDestino("ThymeLeaf");
+		transaccionTO.setMoneda("804");
+		transaccionTO.setMonto(1000);
+		
+		ctx.setVariable("transaccion", transaccionTO);
+		htmlContent = this.templateEngine.process("transferencia", ctx);
+		LOG.debug(htmlContent);
+		
+		documentoHTML = Jsoup.parse(htmlContent);
+		
 	}
 
-	@Test
+	/*@Test
 	public void generateHTML() {
 
 		// Prepare the evaluation context
@@ -46,32 +86,30 @@ public class ThymeleafTest {
 
 		LOG.debug(htmlContent);
 
-	}
+	}*/
 
 	@Test
-	public void transferTest() {
-		final Context ctx = new Context(locale_default);
+	public void verificarNombreCliente() {
 		
-		TransaccionTO transaccionTO = new TransaccionTO();
-		transaccionTO.setName("Manuel");
-		transaccionTO.setLastName("Zegarra");
-		transaccionTO.setFecha(Calendar.getInstance().getTime());
-		transaccionTO.setNumberTx("122");
-		transaccionTO.setCuentaCargo("1122333999");
-		transaccionTO.setCuentaCargoMoneda("804");
-		transaccionTO.setCuentaCargoAlias("Simple");
-		transaccionTO.setCuentaDestino("11223344557");
-		transaccionTO.setNombreDestino("ThymeLeaf");
-		transaccionTO.setMoneda("804");
-		transaccionTO.setMonto(1000);
+		Element table = documentoHTML.select("table.invoice").first();
+		Element row = table.select("tr").get(0);
+		Element col = row.select("td").get(0);
+		String label = col.text();
 		
+		assertEquals("Verificando Nombre Cliente",label,"Manuel");
 		
-		ctx.setVariable("transaccion", transaccionTO);
-		final String htmlContent = this.templateEngine.process("transferencia", ctx);
-
-		assertNotNull(htmlContent);
-
-		LOG.debug(htmlContent);
+	}
+	
+	@Test
+	public void verificarFechaHora() {
+		
+		Element table = documentoHTML.select("table.invoice").first();
+		Element row = table.select("tr").get(1);
+		Element col = row.select("td").get(0);
+		String label = col.text();
+		
+		assertEquals("Verificando Fecha Transaccion",label,"27/09/1991 20:29");
+		
 	}
 
 }
